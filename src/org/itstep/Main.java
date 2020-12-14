2babdb02dcc9f39ee3f9a9d4b69cdcb36809d0a8
student <student@itstep1.dp.ua>
2020-12-14 12:28:35 +0200

    Initial commit

---
+++ src/org/itstep/Main.java
@@ -0,0 +1,7 @@
+package org.itstep;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/org/itstep/Vertex.java
@@ -0,0 +1,27 @@
+package org.itstep;
+
+public class Vertex {
+    private char label;
+    private boolean isVisited;
+
+    public Vertex(char label) {
+        this.label = label;
+        this.isVisited = isVisited;
+    }
+
+    public char getLabel() {
+        return label;
+    }
+
+    public void setLabel(char label) {
+        this.label = label;
+    }
+
+    public boolean isVisited() {
+        return isVisited;
+    }
+
+    public void setVisited(boolean visited) {
+        isVisited = visited;
+    }
+}
